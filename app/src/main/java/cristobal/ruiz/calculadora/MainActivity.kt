71d263ba2a3042ca4e654ca03271cb019f8c25c7
NuclleaR <cristobalruiz1994@gmail.com>
2023-09-09 20:33:33 -0300

    Correccion de bug mensaje error, añadido icono de aplicacion, Validacion de operaciones sin numeros

--- app/src/main/java/cristobal/ruiz/calculadora/MainActivity.kt
+++ app/src/main/java/cristobal/ruiz/calculadora/MainActivity.kt
@@ -5,19 +5,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.content.pm.PackageManager;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraManager;
+import android.widget.Toast
+
 
 
 
 
 class MainActivity : AppCompatActivity() {
     //0-> Nada, 1->Suma, 2->Resta, 3->Multiplicacion, 4->Division
-
-    private lateinit var cameraManager: CameraManager
-    private lateinit var cameraId: String
-    private var isFlashOn = false
 
 
     lateinit var tv_num1: TextView
@@ -31,48 +26,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        // Verificar si el dispositivo tiene cámara con flash
-        val packageManager = packageManager
-        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
-            // Tu dispositivo no tiene flash, realiza alguna acción apropiada
-            // o muestra un mensaje de error.
-        }
-
-        // Inicializar el administrador de la cámara
-        cameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
-
-        // Obtener el ID de la cámara trasera con flash
-        try {
-            cameraId = cameraManager.cameraIdList[0]
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
-
-        val toggleButton = findViewById<Button>(R.id.toggleButton)
-        toggleButton.setOnClickListener(View.OnClickListener {
-            try {
-                if (isFlashOn) {
-                    // Apagar la linterna
-                    cameraManager.setTorchMode(cameraId, false)
-                    isFlashOn = false
-                    toggleButton.text = "Encender Linterna"
-                } else {
-                    // Encender la linterna
-                    cameraManager.setTorchMode(cameraId, true)
-                    isFlashOn = true
-                    toggleButton.text = "Apagar Linterna"
-                }
-            } catch (e: CameraAccessException) {
-                e.printStackTrace()
-            }
-        })
-
-
-
-
-
-    tv_num1 = findViewById(R.id.tv_num1)
+        tv_num1 = findViewById(R.id.tv_num1)
         tv_num2 = findViewById(R.id.tv_num2)
         val btnBorrar: Button = findViewById(R.id.btnC)
         btnigual = findViewById(R.id.btnIgual)
@@ -112,87 +66,107 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun pressButtons(view: View) {
-        var num2: String = tv_num2.text.toString()
+        if(tv_num2.text == "Error") {
+            tv_num2.text = ""
+            var num2: String = tv_num2.text.toString()
 
-        when (view.id) {
-            R.id.btn0 -> tv_num2.setText(num2 + "0")
-            R.id.btn1 -> tv_num2.setText(num2 + "1")
-            R.id.btn2 -> tv_num2.setText(num2 + "2")
-            R.id.btn3 -> tv_num2.setText(num2 + "3")
-            R.id.btn4 -> tv_num2.setText(num2 + "4")
-            R.id.btn5 -> tv_num2.setText(num2 + "5")
-            R.id.btn6 -> tv_num2.setText(num2 + "6")
-            R.id.btn7 -> tv_num2.setText(num2 + "7")
-            R.id.btn8 -> tv_num2.setText(num2 + "8")
-            R.id.btn9 -> tv_num2.setText(num2 + "9")
+
+            when (view.id) {
+                R.id.btn0 -> tv_num2.setText(num2 + "0")
+                R.id.btn1 -> tv_num2.setText(num2 + "1")
+                R.id.btn2 -> tv_num2.setText(num2 + "2")
+                R.id.btn3 -> tv_num2.setText(num2 + "3")
+                R.id.btn4 -> tv_num2.setText(num2 + "4")
+                R.id.btn5 -> tv_num2.setText(num2 + "5")
+                R.id.btn6 -> tv_num2.setText(num2 + "6")
+                R.id.btn7 -> tv_num2.setText(num2 + "7")
+                R.id.btn8 -> tv_num2.setText(num2 + "8")
+                R.id.btn9 -> tv_num2.setText(num2 + "9")
+            }
+        } else{
+            var num2: String = tv_num2.text.toString()
+
+
+            when (view.id) {
+                R.id.btn0 -> tv_num2.setText(num2 + "0")
+                R.id.btn1 -> tv_num2.setText(num2 + "1")
+                R.id.btn2 -> tv_num2.setText(num2 + "2")
+                R.id.btn3 -> tv_num2.setText(num2 + "3")
+                R.id.btn4 -> tv_num2.setText(num2 + "4")
+                R.id.btn5 -> tv_num2.setText(num2 + "5")
+                R.id.btn6 -> tv_num2.setText(num2 + "6")
+                R.id.btn7 -> tv_num2.setText(num2 + "7")
+                R.id.btn8 -> tv_num2.setText(num2 + "8")
+                R.id.btn9 -> tv_num2.setText(num2 + "9")
+            }
         }
+
     }
 
     fun clickOperation(view: View) {
-        numero1 = tv_num2.text.toString().toDouble()
         var numero2_text: String = tv_num2.text.toString()
         val s = "+"
         val r = "-"
         val m = "*"
         val d = "/"
-        if (tv_num1.text.contains(s) || tv_num1.text.contains(r) || tv_num1.text.contains(m) || tv_num1.text.contains(d)){
-                val numero2Text = tv_num2.text.toString()
-                val numero2 = numero2Text.toDoubleOrNull()
 
-                if (numero2 != null) {
-                    var resp: Double = 0.0
+        try {
+            numero1 = tv_num2.text.toString().toDouble()
+        } catch (e: NumberFormatException) {
+            Toast.makeText(this, "Entrada no válida", Toast.LENGTH_SHORT).show()
+            return
+        }
 
-                    when (oper) {
-                        1 -> resp = especial + numero2
-                        2 -> resp = especial - numero2
-                        3 -> resp = especial * numero2
-                        4 -> {
-                            if (numero2 != 0.0) {
-                                resp = especial / numero2
+        if (tv_num1.text.contains(s) || tv_num1.text.contains(r) || tv_num1.text.contains(m) || tv_num1.text.contains(d)) {
+            val numero2Text = tv_num2.text.toString()
+            val numero2 = numero2Text.toDoubleOrNull()
 
-                            } else {
-                                resp = Double.NaN // Nan me permite mostrar un resultado no valido
-                            }
+            if (numero2 != null) {
+                var resp: Double = 0.0
+
+                when (oper) {
+                    1 -> resp = especial + numero2
+                    2 -> resp = especial - numero2
+                    3 -> resp = especial * numero2
+                    4 -> {
+                        if (numero2 != 0.0) {
+                            resp = especial / numero2
+                        } else {
+                            resp = Double.NaN
                         }
-
                     }
-                    especial = 0.0
-                    val resultadoFormateado = mostrarNumero(resp)
-                    tv_num2.text = resultadoFormateado
-                    tv_num1.text = ""
                 }
 
-        }else{
+                especial = 0.0
+                val resultadoFormateado = mostrarNumero(resp)
+                tv_num2.text = resultadoFormateado
+                tv_num1.text = ""
+            }
+        } else {
+
             especial = tv_num2.text.toString().toDouble()
             tv_num2.setText("")
             when (view.id) {
                 R.id.Suma -> {
-
                     tv_num1.setText(numero2_text + "+")
                     oper = 1
                 }
-
                 R.id.Resta -> {
                     tv_num1.setText(numero2_text + "-")
                     oper = 2
                 }
-
                 R.id.Mult -> {
                     tv_num1.setText(numero2_text + "*")
                     oper = 3
                 }
-
                 R.id.Division -> {
                     tv_num1.setText(numero2_text + "/")
                     oper = 4
                 }
-
             }
         }
-
-
-
     }
+
 
     fun mostrarNumero(numero: Double): String {
         if (numero.isNaN()) {
@@ -205,21 +179,3 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
-
-
-
-  /*  fun pressBtn1 (view:View){
-        var txt2 : TextView = findViewById(R.id.tv_num2)
-
-        txt2.text = txt2.text.toString() + "1"
-    }
-    fun pressBtn2 (view:View){
-        var txt2: TextView = findViewById(R.id.tv_num2)
-
-        txt2.text = txt2.text.toString() + "2"
-    }*/
-
-
-
-
-
